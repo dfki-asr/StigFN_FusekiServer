08678c843ce152d326da7ad0e88b49598d189d3d
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2021-07-26 15:17:42 +0200

    Removed Sneaky Throws

--- src/main/java/de/dfki/asr/stigFN/diffusion_distance.java
+++ src/main/java/de/dfki/asr/stigFN/diffusion_distance.java
@@ -1,7 +1,6 @@
 package de.dfki.asr.stigFN;
 
 import java.util.HashMap;
-import lombok.SneakyThrows;
 import org.apache.jena.sparql.expr.NodeValue;
 import java.util.Map;
 import org.apache.jena.sparql.function.FunctionBase5;
@@ -10,7 +9,6 @@ public class diffusion_distance extends FunctionBase5 {
 
     private final Map<String, Concentration> concentrations = new HashMap<>();
 
-    @SneakyThrows
     @Override
     public NodeValue exec(NodeValue sourceId, NodeValue distance, NodeValue duration, NodeValue concentration, NodeValue rate) {
 
