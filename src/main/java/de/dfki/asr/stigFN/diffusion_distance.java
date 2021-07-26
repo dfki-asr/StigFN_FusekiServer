6caef4316b6dac121086e50d764edf5b01e715c8
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2021-07-26 15:15:43 +0200

    Made access to concentration map synchronized

--- src/main/java/de/dfki/asr/stigFN/diffusion_distance.java
+++ src/main/java/de/dfki/asr/stigFN/diffusion_distance.java
@@ -14,9 +14,22 @@ public class diffusion_distance extends FunctionBase5 {
     @Override
     public NodeValue exec(NodeValue sourceId, NodeValue distance, NodeValue duration, NodeValue concentration, NodeValue rate) {
 
-	Concentration conc = concentrations.getOrDefault(sourceId.asString(), new Concentration());
+	if (!concentrations.containsKey(sourceId.asString())) {
+	    concentrations.put(sourceId.asString(), new Concentration());
+	}
+	Concentration conc = concentrations.get(sourceId.asString());
 
 	double output = conc.getAtDistance(distance, duration, concentration, rate);
 	return NodeValue.makeDouble(output);
+    }
+
+    private synchronized Concentration getAndAddIfNotExist(String sourceId) {
+	if (!concentrations.containsKey(sourceId)) {
+	    System.out.println("[diffusion_distance] Source Stigma with ID " + sourceId + " not yet present. Adding.");
+	    concentrations.put(sourceId, new Concentration());
+	}
+
+	System.out.println("[diffusion_distance] Returning Source Stigma with ID " + sourceId);
+	return concentrations.get(sourceId);
     }
 }
