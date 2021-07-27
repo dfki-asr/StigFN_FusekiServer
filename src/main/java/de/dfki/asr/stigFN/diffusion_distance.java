218bd27f101cc3f138812bd067a77dbd8cc59da0
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2021-07-27 09:25:08 +0200

    Added try catch block as Fuseki seems to suppress all uncaught exceptions

--- src/main/java/de/dfki/asr/stigFN/diffusion_distance.java
+++ src/main/java/de/dfki/asr/stigFN/diffusion_distance.java
@@ -8,9 +8,13 @@ public class diffusion_distance extends FunctionBase5 {
     @Override
     public NodeValue exec(NodeValue sourceId, NodeValue distance, NodeValue duration, NodeValue concentration, NodeValue rate) {
 
-	Concentration conc = ConcentrationsManager.getAndAddIfNotExist(sourceId.asString());
-
-	double output = conc.getAtDistance(distance, duration, concentration, rate);
-	return NodeValue.makeDouble(output);
+	try {
+	    Concentration conc = ConcentrationsManager.getAndAddIfNotExist(sourceId.asString());
+	    double output = conc.getAtDistance(distance, duration, concentration, rate);
+	    return NodeValue.makeDouble(output);
+	} catch (Exception e) {
+	    System.out.println("[diffusion_distance] An exception occured during diffusion_distance.exec: " + e.getMessage());
+	}
+	return NodeValue.makeDouble(0.0);
     }
 }
