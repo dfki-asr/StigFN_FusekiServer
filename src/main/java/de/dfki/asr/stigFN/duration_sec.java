2e1a3789bea124d3d04ece75a1d0f6ff6bbdbf1a
Melvin Chelli <Melvin.Chelli@dfki.de>
2021-09-21 07:36:34 +0200

    Catch parse exception in date time

--- src/main/java/de/dfki/asr/stigFN/duration_sec.java
+++ src/main/java/de/dfki/asr/stigFN/duration_sec.java
@@ -10,19 +10,24 @@ import java.util.Date;
 public class duration_sec extends FunctionBase2 {
     @Override
     public NodeValue exec(NodeValue nodeValue, NodeValue nodeValue1) {
+        
         String t1 = ((nodeValue.toString()).split("\\^")[0].replace("T", " ").split("\\+")[0].split("\"")[1]);
         String t2 = ((nodeValue1.toString()).split("\\^")[0].replace("T", " ").split("\\+")[0].split("\"")[1]);
+        
         DateFormat df = new java.text.SimpleDateFormat("yyyy-mm-dd HH:mm:ss.SSS");
         Date date1 = null;
         Date date2 = null;
-
+        double diff;
         try {
             date1 = df.parse(t1);
             date2 = df.parse(t2);
+            diff = (date2.getTime() - date1.getTime())/1000.0;
         } catch (ParseException e) {
+            diff = 10;
             e.printStackTrace();
         }
-        double diff = (date2.getTime() - date1.getTime())/1000.0;
+        
+        
 
         return NodeValue.makeDouble(diff);
     }
